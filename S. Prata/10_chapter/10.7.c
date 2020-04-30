6e57ee26c6af3fd4198f19ce81e0d339307a9f57
vladimir-del <vladimirdraga@gmail.com>
2020-04-30 18:26:45 +1000

    add 10.7

---
+++ S. Prata/10_chapter/10.7.c
@@ -0,0 +1,17 @@
+//-------------------------------------------------
+//Date: 30.04.2020
+//Autor: Vlaimir Draga
+//S. Prata. Chapter 10
+// Programming exercise 7
+//-------------------------------------------------
+
+/* 
+Text programmming exersice.
+
+Write a program that initializes a two-dimensional array-of- double and uses one of the
+copy functions from exercise 2 to copy it to a second two-dimensional array. (Because a
+two-dimensional array is an array of arrays, a one-dimensional copy function can be used
+with each subarray.)
+*/
+
+#include <stdio.h>
