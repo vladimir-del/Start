b71beb9228a32dc40c5e70285ef39778adc7a8fa
vladimir-del <vladimirdraga@gmail.com>
2020-05-02 12:54:36 +1000

    modified:   S. Prata/10_chapter/10.7.c

--- S. Prata/10_chapter/10.7.c
+++ S. Prata/10_chapter/10.7.c
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-//-------------------------------------------------
-//Date: 30.04.2020
-//Autor: Vlaimir Draga
-//S. Prata. Chapter 10
-// Programming exercise 7
-//-------------------------------------------------
-
-/* 
-Text programmming exersice.
-
-Write a program that initializes a two-dimensional array-of- double and uses one of the
-copy functions from exercise 2 to copy it to a second two-dimensional array. (Because a
-two-dimensional array is an array of arrays, a one-dimensional copy function can be used
-with each subarray.)
-*/
-
-#include <stdio.h>
-=======
 //-------------------------------------------------
 //Date: 29.01.2020
 //Autor: Vlaimir Draga
@@ -65,4 +46,3 @@ void func_cpy(int num_row, int num_col, double ArrTo[num_row][num_col], double (
 			*(*(ArrTo + i) + l) = *(*(ArrFrom + i) + l);
 	}
 }
->>>>>>> 5936f95de321e2feec3a398439b75355032620dd
