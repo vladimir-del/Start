3818e2bbe87e8ad81bfb715b87c138005a2fb91b
vladimir-del <56100237+vladimir-del@users.noreply.github.com>
2020-09-21 15:43:59 +1000

    Update 11.11.c
    My results from work.

--- S. Prata/11_chapter/11.11.c
+++ S. Prata/11_chapter/11.11.c
@@ -21,25 +21,105 @@ actually perform the promised tasks.
 #include <limits.h>
 
 #define NUM_STR 10
-#define MAX_LENGHT 80
 
-int main (void)
+
+void func1(void);
+void func2(void);
+void func3(void);
+void func4(void);
+void func5(void);
+
+int main(int argc, char * argv[])
 {
-    char str1[LINE_MAX];
-    //int i = 1;
-    //int c;
+	char const *quit = "quit\n";
+	int MenuSelect;
+	
+	printf("If you want to exit. Write quit\n");
+	
+	char string[NUM_STR][LINE_MAX];
 
-    /*  do
-    {
-         
-        printf("S%d = ", i);
-        fgets(str1, LINE_MAX, stdin);
-        printf("string after function *delete_space = %s", str1);
-        i++;
+		for (int i = 0; i < NUM_STR ; i++)
+		{
+			printf("String[%d] = ", i);
+			fgets(string[i], LINE_MAX, stdin);
+						
+			//check for exit
+			if (strcmp(string[i], quit) == 0)
+			{
+				printf(" string[%d] = %s", i, string[i][LINE_MAX]);
+				break;
+			}
+		}
+		
+	
+		
 
-    }
-    */
-     while (fgets(str1, LINE_MAX, stdin) != NULL);
-        printf("End program. You enter an empty string\n");
-        printf("%s", str1);
+
+
+	while (scanf("%d", &MenuSelect)!=5)
+	{
+		printf("\n\nPlease select the appropriate item:\n\n");
+		printf("(1) print the original list of strings\n");
+		printf("(2) print the strings in ASCII collating sequence\n");
+		printf("(3) print the strings in order of increasing length\n");
+		printf("(4) print the strings in order of the length of the first word in the string\n");
+		printf("(5) quit\n");
+		
+		printf("%d\n", MenuSelect);
+		
+		switch (MenuSelect)
+		{
+		case 1:
+			func1();
+			break;
+		case 2:
+			func2();
+			break;
+			
+		case 3:
+			func3();
+			break;
+			
+		case 4:
+			func4();
+			break;
+			
+		case 5:
+			
+			break;
+				
+		default:
+			printf("Wrong input\n");
+			//scanf("%d", &MenuSelect);
+			break;
+		}
+
+	}
+
+	
+}
+
+void func1(void)
+{
+	printf("func 1\n");
+}
+
+void func2(void)
+{
+	printf("func 2\n");
+}
+
+void func3(void)
+{
+	printf("func 3\n");
+}
+
+void func4(void)
+{
+	printf("func 4\n");
+}
+
+void func5(void)
+{
+	printf("func 5\n");
 }
