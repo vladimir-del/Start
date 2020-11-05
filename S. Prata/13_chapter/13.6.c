4cf9e96674416260624219aac542ea7f2014e7a4
vladimir-del <56100237+vladimir-del@users.noreply.github.com>
2020-11-06 09:59:38 +1000

    Update 13.6.c
    Solved 13.6.c

--- S. Prata/13_chapter/13.6.c
+++ S. Prata/13_chapter/13.6.c
@@ -15,29 +15,27 @@ arguments, it prompts the user for the required information.
 #include <stdlib.h>
 #include <string.h>
 
+#define LEN 81
+
 int main(int argc, char *argv[])
 {
     FILE *in, *out; // declare two FILE pointers
     int ch;
+	char file_src[LEN];
     char name[LEN]; // storage for output filename
     int count = 0; // check for command-line arguments
 
-    if (argc < 2)
+	puts("Enter source file name");
+	scanf("%s", file_src);
+    if ((in = fopen(file_src, "r")) == NULL)
         {
-            fprintf(stderr, "Usage: %s filename\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-
-    // set up input
-    if ((in = fopen(argv[1], "r")) == NULL)
-        {
-            fprintf(stderr, "I couldn't open the file \"%s\"\n",
-            argv[1]);
+            fprintf(stderr, "I couldn't open the file \"%s\"\n", file_src);
             exit(EXIT_FAILURE);
         }
 
     // set up output
-    strncpy(name,argv[1], LEN - 5); // copy filename
+	
+    strncpy(name,file_src, LEN - 5); // copy filename
     name[LEN - 5] = '\0';
     strcat(name,".red");
 
