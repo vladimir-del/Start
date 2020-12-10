726b7426a1c45b6652d41eafc45a9084839cf3ed
vladimir-del <56100237+vladimir-del@users.noreply.github.com>
2020-12-10 13:36:20 +1000

    Update 14.5.c

--- S. Prata/14_chapter/14.5.c
+++ S. Prata/14_chapter/14.5.c
@@ -7,28 +7,21 @@
 /* 
 Text programmming exersice.
 5. Write a program that fits the following recipe:
-
 a. Externally define a name structure template with two members: a string to hold the
 first name and a string to hold the second name.
-
 b. Externally define a student structure template with three members: a name
 structure, a grade array to hold three floating-point scores, and a variable to hold
 the average of those three scores.
-
 c. Have the main() function declare an array of CSIZE (with CSIZE = 4 ) student
 structures and initialize the name portions to names of your choice. Use functions
 to perform the tasks described in parts d., e., f., and g.
-
 d. Interactively acquire scores for each student by prompting the user with a student
 name and a request for scores. Place the scores in the grade array portion of the
 appropriate structure. The required looping can be done in main() or in the
 function, as you prefer.
-
 e. Calculate the average score value for each structure and assign it to the proper
 member.
-
 f. Print the information in each structure.
-
 g. Print the class average for each of the numeric structure members.
 */
 
@@ -49,8 +42,10 @@ struct student {
 };
 
 int getname(struct student *);
+void inputgrades(struct student *);
 void printstruct(const struct student *, int n);
 char * s_gets(char * st, int n);
+void averagegrade(struct student *);
 
 int main(void)
 {
@@ -65,7 +60,9 @@ int main(void)
 
 printstruct(CSIZE, 4);
 
-printf("%d", getname(CSIZE));
+inputgrades(CSIZE);
+
+printstruct(CSIZE, 4);
 
 
 	return 0;
@@ -77,8 +74,9 @@ void printstruct(const struct student *s, int n)
     for (int i = 0; i < n; i++)
     {
     printf("Name Student:%s %s\n", (s+i)->name.FirstName, (s+i)->name.LastName);
-    printf("His grades:%0.1f, %0.1f, %0.1f\n", (s+i)->grade[0],(s+i)->grade[1], (s+i)->grade[2]);
-    puts("");
+    printf("His grades:%0.2f, %0.2f, %0.2f\n", (s+i)->grade[0],(s+i)->grade[1], (s+i)->grade[2]);
+    printf("His averagegrade = %.2f\n", (s + i)->AverageGrade);
+	puts("");
     }
 }
 
@@ -117,4 +115,27 @@ int getname(struct student *s)
         }   
     }
     return -1;
+}
+
+void inputgrades(struct student *s)
+{
+	int n = getname(s);
+	printf("input three grades:\n");
+	for (int i = 0; i < 3; i++)
+	{
+		printf("Grade %d = ");
+		scanf("%f", &(s + n)->grade[i]);
+	}
+	averagegrade(s);
+}
+
+void averagegrade(struct student *s)
+{
+	float temp[4] = {0};
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0 ; j < 3; j++)
+			temp[i] += (s + i)->grade[j];
+	(s + i)->AverageGrade = temp[i]/3;
+	}
 }
