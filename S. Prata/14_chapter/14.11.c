fa0a6084658d9f0131b7b25d6ca32590d698b267
vladimir-del <vladimirdraga@gmail.com>
2020-12-12 19:30:40 +1000

    solved 14.11

--- S. Prata/14_chapter/14.11.c
+++ S. Prata/14_chapter/14.11.c
@@ -24,23 +24,112 @@ of the transform() function.
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
+
+#define N 10
 
 void transform (double *source, double *target, int n, double (*fp)(double));
+void printarr(double *target, int n);
+double sum(double);
+double del(double);
+
+char showmenu(void);
 
 int main(void)
 {
-    double source[2] = {0.1, 0.2};
-    double target[2] = {0};
-    printf("%.2f\n", target[0]);
-    transform(source, target, 2, sin);
 
-    printf("%.2f\n", target[0]);
+    double (* fp[4])(double) = {sin, cos, sum, del};
+    double source[N] = {90, 0.2, 3, 2.3, 15, 5, 0.2, 0.6, 0.5, 1.1};
+    double target[N] = {0};
+    char choice;
+
+    while((choice = showmenu()) != 'n')
+    {
+        switch (choice)
+        {
+        case 'a': 
+        system("clear");
+        printf("Target arrau after sin\n");
+        transform(source, target, N, fp[0]);
+        printarr(target, N);
+        printf("Press Enter to continue");
+        getchar(); getchar();
+            break;
+        
+        case 'b': 
+        system("clear");
+        printf("Target arrau after cos\n");
+        transform(source, target, N, fp[1]);
+        printarr(target, N);
+        printf("Press Enter to continue");
+        getchar(); getchar();
+            break;
+        
+        case 'c': 
+        system("clear");
+        printf("Target arrau after sum\n");
+        transform(source, target, N, fp[2]);
+        printarr(target, N);
+        printf("Press Enter to continue");
+        getchar(); getchar();
+            break;
+        
+        case 'd': 
+        system("clear");
+        printf("Target arrau after del\n");
+        transform(source, target, N, fp[3]);
+        printarr(target, N);
+        printf("Press Enter to continue");
+        getchar(); getchar();
+            break;
+        
+        default:
+            break;
+        }
+
+        system("clear");
+
+    }
+
+    transform(source, target, N, sin);
+
+    printarr(target, N);
     return 0;
 }
 
 void transform (double *source, double *target, int n, double (*fp)(double))
 {
-    target[0] =  (*fp)(source[0]);
-    return;
 
+    for (int i = 0; i < n; i++)
+         *(target + i) =  (*fp)(*(source + i));
+}
+
+void printarr(double *target, int n)
+{
+    for (int i = 0; i < n; i++)
+    printf("target[%d] = %.1f \n", i, *(target + i));
+}
+
+char showmenu(void)
+{
+    char answer;
+    puts("Choice func");
+    printf("(a) sin\n(b) cos\n(c) sum\n(d) del\n");
+    answer = getchar();
+    while (strchr("abcd", answer) == NULL)
+    {
+        printf("Input only a, b, c, d\n");
+        answer = getchar();
+    }
+    return answer;
+}
+
+double sum(double n)
+{
+   return n += 2;
+}
+
+double del(double n)
+{
+    return n /= 2;
 }
